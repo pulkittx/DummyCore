6a1bb220cf9ae52fd96b14d8cc1490ba792d7b25
Pulkit Agarwal <pulkit.agarwal@testingxperts.com>
2021-10-20 19:17:31 +0530

    Added new project.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace DummyFrame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#########################################################");
+         
+            IWebDriver webDriver = new ChromeDriver(@"C:\Users\sumit\source\repos\DummyFrame");
+            webDriver.Manage().Window.Maximize();
+            webDriver.Url = "https://www.tcw.com/";
+        }
+    }
+}
